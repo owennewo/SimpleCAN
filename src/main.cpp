7674a35b4d3bbfc7d75a683eddda9d7ffdd73344
Owen Williams <williams.owen@gmail.com>
2021-02-28 21:43:17 +0000

    tidying up #1

--- src/main.cpp
+++ src/main.cpp
@@ -48,111 +48,6 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
 
 }
 
-
-void NMI_Handler(void)
-{
-  while(true) ;
-}
-
-
-void HardFault_Handler(void)
-{
-while(true) ;
-}
-
-/**
-  * @brief This function handles Memory management fault.
-  */
-void MemManage_Handler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles Pre-fetch fault, memory access fault.
-  */
-void BusFault_Handler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles Undefined instruction or illegal state.
-  */
-void UsageFault_Handler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles System service call via SWI instruction.
-  */
-void SVC_Handler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles Debug monitor.
-  */
-void DebugMon_Handler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{
- while(true) ;
-}
-
-/**
-  * @brief This function handles System tick timer.
-  */
-// void SysTick_Handler(void)
-// {
-//   while(true) ;
-// }
-
-/******************************************************************************/
-/* STM32F4xx Peripheral Interrupt Handlers                                    */
-/* Add here the Interrupt Handlers for the used peripherals.                  */
-/* For the available peripheral interrupt handler names,                      */
-/* please refer to the startup file (startup_stm32f4xx.s).                    */
-/******************************************************************************/
-
-
-
-void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan) {
-    // The notification will not refire immidiately so we don't need to disable it.
-    //get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_tx_complete<0>));
-    // get_can(hcan).on_tx_complete(0);
-}
-
-void HAL_CAN_TxMailbox1CompleteCallback(CAN_HandleTypeDef *hcan) {
-    //get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_tx_complete<1>));
-    // get_can(hcan).on_tx_complete(1);
-}
-
-void HAL_CAN_TxMailbox2CompleteCallback(CAN_HandleTypeDef *hcan) {
-    //get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_tx_complete<2>));
-    // get_can(hcan).on_tx_complete(2);
-}
-
-void HAL_CAN_TxMailbox0AbortCallback(CAN_HandleTypeDef *hcan) {
-    // get_can(hcan).on_tx_abort(0);
-}
-
-void HAL_CAN_TxMailbox1AbortCallback(CAN_HandleTypeDef *hcan) {
-    // get_can(hcan).on_tx_abort(1);
-}
-
-void HAL_CAN_TxMailbox2AbortCallback(CAN_HandleTypeDef *hcan) {
-    // get_can(hcan).on_tx_abort(2);
-}
-
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     // HAL_CAN_DeactivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
     // get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_rx_fifo_pending<CAN_RX_FIFO0>));
@@ -160,69 +55,8 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
   Serial.println(".");
   CAN_RxHeaderTypeDef RxHeader;
   uint8_t RxData[8]  = {0};
-  HAL_CAN_GetRxMessage(&can.hcan, CAN_RX_FIFO0, &RxHeader, RxData);
+  HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData);
   
-}
-
-//void HAL_CAN_RxFifo0FullCallback(CAN_HandleTypeDef *hcan) {
-//    get_can(hcan).on_rx_fifo_full(0);
-//}
-
-void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan) {
-  Serial.println(".");
-    // HAL_CAN_DeactivateNotification(hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
-    // get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_rx_fifo_pending<CAN_RX_FIFO1>));
-}
-
-//void HAL_CAN_RxFifo1FullCallback(CAN_HandleTypeDef *hcan) {
-//    get_can(hcan).on_rx_fifo_full(1);
-//}
-
-void HAL_CAN_SleepCallback(CAN_HandleTypeDef *hcan) {
-    // nothing to do
-}
-
-void HAL_CAN_WakeUpFromRxMsgCallback(CAN_HandleTypeDef *hcan) {
-    // nothing to do
-}
-
-void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan) {
-    // get_can(hcan).on_event_.invoke(MEMBER_CB(&get_can(hcan), on_error));
-}
-
-
-
-
-/**
-  * @brief This function handles PVD interrupt through EXTI line 16.
-  */
-void PVD_IRQHandler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles Flash global interrupt.
-  */
-void FLASH_IRQHandler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles RCC global interrupt.
-  */
-void RCC_IRQHandler(void)
-{
-  while(true) ;
-}
-
-/**
-  * @brief This function handles CAN1 TX interrupts.
-  */
-void CAN1_TX_IRQHandler(void)
-{
-  while(true) ;
 }
 
 /**
@@ -231,38 +65,8 @@ void CAN1_TX_IRQHandler(void)
 void CAN1_RX0_IRQHandler(void)
 {
   HAL_CAN_IRQHandler(&can.hcan);
-  
-  Serial.println("+");
 }
-
 }
-// /**
-//   * @brief This function handles CAN1 RX1 interrupt.
-//   */
-// void CAN1_RX1_IRQHandler(void)
-// {
-//   while(true) ;
-// }
-
-// /**
-//   * @brief This function handles CAN1 SCE interrupt.
-//   */
-// void CAN1_SCE_IRQHandler(void)
-// {
-//   while(true) ;
-// }
-
-// void WWDG_IRQHandler(void) {
-// while(true) ;
-// }
-
-// /**
-//   * @brief This function handles FPU global interrupt.
-//   */
-// void FPU_IRQHandler(void)
-// {
-//   while(true) ;
-// }
 
 
 void setup() {
